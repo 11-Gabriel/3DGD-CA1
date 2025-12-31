08556093736c88e4d4ed5446ca5d4de4f46c8e26
Gabriel Wong <gabwong9999@gmail.com>
2025-12-31 16:36:50 +0800

    Deleted keys script

--- 3DGD CA1/Assets/Scripts/Keys.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Keys : MonoBehaviour
-{
-    // This script is now empty - key collection logic moved to PlayerMovement
-    // Keep this component on key objects if needed for other functionality
-}
